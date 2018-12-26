6246580ec5dd495b302a56259d446f7d11d9fd2f
Dislanet <vdisla@gmail.com>
2018-12-26 06:54:27 -0400

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.dislacode.webviewsite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.myWebView);
+
+        WebSettings webSettings = myWebView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.loadUrl("https://dislanet.com");
+
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (myWebView.canGoBack())
+        {
+            myWebView.goBack();
+
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
